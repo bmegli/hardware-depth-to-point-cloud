4dc369d8f9c7e3cfabb9e1bae6702d0f9a86bf88
Bartosz Meglicki <bmegli@op.pl>
2020-11-23 21:32:09 +0100

    visualize boundary

--- hdu.c
+++ hdu.c
@@ -13,6 +13,7 @@
 
 #include <stdio.h> //fprintf
 #include <stdlib.h> //malloc
+#include <math.h> //fabs
 
 //in binary 10 ones followed by 6 zeroes
 static const uint16_t P010LE_MAX = 0xFFC0;
@@ -62,6 +63,12 @@ void hdu_unproject(const struct hdu *h, const struct hdu_depth *depth, struct hd
 {
 	const int pc_size = pc->size;
 	const color32 default_color = 0xFFFFFFFF;
+	const color32 red_color = 0xFF0000FF;
+	const color32 blue_color = 0xFFFF0000;
+	const float z_move_m = 0.50f / 30.0f;
+	const float sqrt3 = sqrt(3);
+	const float baseline_m = 0.095;
+
 	int points=0;
 	float d;
 
@@ -77,7 +84,14 @@ void hdu_unproject(const struct hdu *h, const struct hdu_depth *depth, struct hd
 			pc->data[points][2] = d;
 
 			const uint32_t *color_line = (uint32_t*)(((uint8_t*)depth->colors) + r * depth->color_stride);
-			pc->colors[points] = depth->colors ? color_line[c] : default_color;
+//			pc->colors[points] = depth->colors ? color_line[c] : default_color;
+
+			pc->colors[points] = (d < pc->data[points][0] + z_move_m) ||
+			                     (d < baseline_m - pc->data[points][0] + z_move_m ) ?
+			                     red_color : color_line[c];
+
+			if(d < fabs(pc->data[points][1]) * sqrt3 + z_move_m)
+				pc->colors[points] = blue_color;
 
 			++points;
 		}
